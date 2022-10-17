cce2eacd1f12f44e63d061a93ac4493480bb209a
honsumal <14leealastair@gmail.com>
2022-10-17 19:06:56 -0400

    added schema, sample seeds

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (id, name)
+VALUES
+    (1, 'R&D'),
+    (2, 'Marketing');
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES
+    (1, 'Manager', 85375.00, 1),
+    (2, 'Engineer', 79520.00, 1),
+    (3, 'Intern', 37793.60, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (1, "Samwell", "Lockwood", 1, NULL),
+    (2, "Candice", "Hartley", 2, 1),
+    (3, "Shigeo", "Shearer", 3, 1)
